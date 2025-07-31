718ab6daee8943ed2a974e7c46eede10579581e4
Graziano Paladino <graziano.paladino@iet.it>
2025-07-31 16:00:06 +0200

    refactoring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { ArticoliComponent } from './modules/articoli/pages/articoli/articoli.component';
 import { AuthGuard } from 'src/app/core/services/route-guard.service';
 import { ErrorComponent } from './modules/errors/pages/error/error.component';
+import { GridArticoliComponent } from './modules/articoli/pages/grid-articoli/grid-articoli.component';
 import { LoginComponent } from './modules/login/pages/login/login.component';
 import { LogoutComponent } from './modules/logout/pages/logout/logout.component';
 import { NgModule } from '@angular/core';
 import { WelcomeComponent } from './modules/home/pages/welcome/welcome.component';
-import { GridArticoliComponent } from './modules/articoli/pages/grid-articoli/grid-articoli.component';
 
 const routes: Routes = [
   {path:'', component: LoginComponent},
   {path:'login', component: LoginComponent},
-  {path:'welcome/:userid', component: WelcomeComponent, canActivate:[AuthGuard]},
   {path:'welcome', component: WelcomeComponent, canActivate:[AuthGuard]},
+  {path:'welcome/:userid', component: WelcomeComponent, canActivate:[AuthGuard]},
   {path:'articoli', component : ArticoliComponent, canActivate:[AuthGuard]},
   {path:'articoli/grid', component : GridArticoliComponent, canActivate:[AuthGuard]},
   {path:'logout', component : LogoutComponent},
---
+++ src/app/modules/articoli/components/articoli-card/articoli-card.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+import { IArticoli } from 'src/app/shared/models/Articoli';
+
+@Component({
+  selector: 'app-articoli-card',
+  templateUrl: './articoli-card.component.html',
+  styleUrls: ['./articoli-card.component.css']
+})
+export class ArticoliCardComponent {
+
+  @Input()
+  articolo: IArticoli  = {
+    codart: '',
+    descrizione: '',
+    um: '',
+    pzcart: 0,
+    peso: 0,
+    prezzo: 0,
+    active: true,
+    data: new Date(),
+    imageUrl: ''
+  };
+
+  @Output()
+  delete = new EventEmitter();
+  @Output()
+  edit = new EventEmitter();
+
+  editArt = () =>  this.edit.emit(this.articolo.codart);
+  delArt = () => this.delete.emit(this.articolo.codart);
+
+}
